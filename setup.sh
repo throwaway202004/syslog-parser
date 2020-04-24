e630d67dbba85ad4277676f39a33adf3bb5a5502
David <David>
2020-04-24 13:07:46 +0200

    Minor changes in setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-file=`find . -name "kafka*.tgz" -type f`
+topic="syslog"
+
+file=`find . -maxdepth 1 -type f -name "kafka*.tgz"`
 
 if [ "$file" ]
 then
@@ -14,31 +16,43 @@ fi
 
 tar -xzf kafka_"$version".tgz
 
+kafka_dir="kafka_$version"
+
 echo "Starting Zookeeper"
-kafka_2.12-2.5.0/bin/zookeeper-server-start.sh kafka_2.12-2.5.0/config/zookeeper.properties > /dev/null 2>&1 &
+"$kafka_dir"/bin/zookeeper-server-start.sh "$kafka_dir"/config/zookeeper.properties > /dev/null 2>&1 &
 
 echo
 echo "Starting server"
-kafka_2.12-2.5.0/bin/kafka-server-start.sh kafka_2.12-2.5.0/config/server.properties > /dev/null 2>&1 &
+"$kafka_dir"/bin/kafka-server-start.sh "$kafka_dir"/config/server.properties > /dev/null 2>&1 &
+
+echo
+echo "Wait some time for broker to start"
+echo
+
+sleep 3
 
 echo
 echo "Creating topic '$topic'"
-kafka_2.12-2.5.0/bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic syslog
+"$kafka_dir"/bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic syslog
 
 echo
 echo "Topic created"
 echo
 echo
 
-#echo "Building Java application"
-#mvn package
-#
+echo "Building Java application"
+mvn clean
+mvn package
 
-# Start the consumer java app
-# Let is print to stdout and also pipe stdout to a file
-#
-#echo "$consumer_cmd"
-#
-# Print instructions of how to run the producer.sh script in another windowd
-#
+echo
+echo "Set up finished"
+echo
+echo "Now, start the consumer process with the command"
+echo
+echo "java -jar target/syslog-parser-1.0-SNAPSHOT-jar-with-dependencies.jar"
+echo
+echo
+echo "Open up another terminal window and run the producer process:"
+echo
+echo "cat syslog.txt | ./producer.sh"
 
